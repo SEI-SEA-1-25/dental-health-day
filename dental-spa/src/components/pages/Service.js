fa41b03c6052839405d5aa2e72641158e765ef6c
ianalexanderz <ianstewart745@gmail.com>
2021-03-15 09:24:55 -0700

    initial commit. need to commit changes much sooner. bad habit.

---
+++ dental-spa/src/components/pages/Service.js
@@ -0,0 +1,9 @@
+const Service = (props) => (
+  <div>
+    <h1>{props.service.name}</h1>
+    <h3>{props.service.description}</h3>
+    <p>${props.service.price}</p>
+  </div>
+)
+
+export default Service;
---
+++ dental-spa/src/data/serviceDetails.js
@@ -0,0 +1,21 @@
+const services = [
+  {
+    id: 1,
+    name: "Drillin' and Fillin'",
+    price: 300,
+    description: "Root canal!",
+  },
+  {
+    id: 2,
+    name: "Molar Massage",
+    price: 500,
+    description: "Relax, teeth. Relaaaaaaax.",
+  },
+  {
+    id: 3,
+    name: "Cucumber Mask (but for your cavities)",
+    price: 350,
+    description: "Your cavities have never been so radiant!",
+  },
+];
+export default services;
